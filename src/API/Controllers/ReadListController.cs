8a52f88f786aae84ea02b7baf2bc3871ef171dd3
Nkem <nkemsileslie@gmail.com>
2022-08-21 15:43:58 +0100

    create user readlist

---
+++ src/API/Controllers/ReadListController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Application.Services;
+using Application.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Application.DataTransferObjects;
+using Application.Helpers;
+
+namespace API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/readlists")]
+    public class ReadListController : ControllerBase
+    {
+       private readonly IServiceManager _service;
+
+        public ReadListController(IServiceManager service)
+        {
+            _service = service;
+        }
+
+        /// <summary>
+        /// Endpoint to invite a new user
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("create/{id}")]
+        [ProducesResponseType(typeof(SuccessResponse<UserReadListDTO>), 200)]
+        public async Task<IActionResult> CreateReadList(Guid id, [FromBody] CreateReadListDTO model)
+        {
+            var response = await _service.SingleUserService.CreateReadList(id, model);
+            return Ok(response);
+        }
+
+    }
+}
---
+++ src/Application/DataTransferObjects/BookDTO.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Application.DataTransferObjects
+{
+    public record ViewBookDTO
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+    }
+}
